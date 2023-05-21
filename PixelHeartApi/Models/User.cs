cfeabe53947006e3a98d69cd1c689b63065f79b6
MePerson1 <112072726+MePerson1@users.noreply.github.com>
2023-05-21 23:12:07 +0200

    Dodaj pliki projektów.

---
+++ PixelHeartApi/Models/User.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PixelHeartApi.Models
+{
+    public class User
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public string Email { get; set; }
+        [Required]
+        public string Password { get; set; } //może to Hashcode powinen byc
+        [Required]
+        public string Backstory { get; set; }
+        [Required]
+        public int Level { get; set; }
+
+        //Stats, favGames,
+
+    }
+}
