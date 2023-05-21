10fe376f37620cb8c12b51a026c042a4b8b127ec
MePerson1 <112072726+MePerson1@users.noreply.github.com>
2023-05-21 23:36:48 +0200

    Dobra tutaj wiele do wielu jednak XD

--- PixelHeartApi/Models/UserGamel.cs
+++ PixelHeartApi/Models/UserGamel.cs
@@ -1,6 +1,19 @@
-﻿namespace PixelHeartApi.Models
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace PixelHeartApi.Models
 {
     public class UserGamel
     {
+        [Key]
+        public int UserGamelId { get; set; }
+        [Required]
+        public int UserId { get; set; }
+        [ForeignKey("UserId")]
+        public User User { get; set; }
+        [Required]
+        public int GameId { get; set; }
+        [ForeignKey("GameId")]
+        public Skill Game { get; set; }
     }
 }
