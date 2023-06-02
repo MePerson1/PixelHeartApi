971040c8eda466b237ba8392f16e1538931bf24b
MePerson1 <112072726+MePerson1@users.noreply.github.com>
2023-06-02 22:07:45 +0200

    qrqweqwe
    wqe

--- PixelHeartApi/Interfaces/IGameRepository.cs
+++ PixelHeartApi/Interfaces/IGameRepository.cs
@@ -9,5 +9,11 @@ namespace PixelHeartApi.Interfaces
         int Create(Game game);
         bool Update(int id, Game game);
         bool Delete(int id);
+
+        ICollection<Game> GetGameByUserId(int userId);
+        ICollection<Game> GetAllGames();
+        ICollection<User> GetUserByGameId(int userId);
+
+
     }
 }
